7a08fe66a0058541c614ce302b400781188a2a2a
William Jin <jinwilliam.jin@gmail.com>
2024-10-25 18:21:01 -0400

    figured out useAnimate

--- anim-practice/use-animate-pract/src/App.jsx
+++ anim-practice/use-animate-pract/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useState } from 'react'
+import { useAnimate } from 'framer-motion'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const Component = () => {
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
 }
 
-export default App
+export default function App() {
+  const [scope, animate] = useAnimate();
+
+
+  async function handleAnimate() {
+    await animate("#target", {
+      x:150,
+    })
+    await animate("#target", {
+      y:150, 
+      rotate:"360deg",
+      
+
+    },
+    {
+      duration: 1.5,
+
+    })
+    await animate(".button", {rotate:"360deg"})
+
+    
+    await Promise.all(
+      [animate("#target", {x:0,y:0,rotate:0,})],
+      [animate(".button", {rotate:0})])
+    
+  }
+
+  useEffect(() => {
+    handleAnimate()
+  }, [])
+  return(
+    <div className='main' ref={scope} >
+      <div id="target" className="purple center"></div>
+      <button 
+      onClick={handleAnimate}
+      className='button'>Trigger Animation</button>
+    </div>
+  )
+}
