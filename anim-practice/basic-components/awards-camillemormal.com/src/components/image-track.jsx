f76704fcbdb72ab0b20761d68238dfff40c06a1c
William Jin <jinwilliam.jin@gmail.com>
2024-10-31 00:06:54 -0400

    working on pan, WIP

--- anim-practice/basic-components/awards-camillemormal.com/src/components/image-track.jsx
+++ anim-practice/basic-components/awards-camillemormal.com/src/components/image-track.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useRef, useState } from "react";
-import { motion, AnimatePresence, useScroll, useTransform, useAnimate} from "framer-motion";
+import { motion, AnimatePresence, useScroll, useTransform, useAnimate } from "framer-motion";
 
 
 const imgList = []
 for (let i = 0; i < 8; i++) {
-    imgList.push(`../../public/img-${i+1}.jpg`)
+    imgList.push(`../../public/img-${i + 1}.jpg`)
 }
 
 export default function Track() {
@@ -13,9 +13,13 @@ export default function Track() {
     const divRef = useRef(null);
     const { scrollYProgress } = useScroll({ target: divRef });
     const [scope, animate] = useAnimate();
-    const [trackX, setTrackX] = useState(null);
-    const [imageX, setImageX] = useState(null);
-    // const scrollPercent = useRef(null);
+    const [trackX, setTrackX] = useState("21.3%");
+    const [imageX, setImageX] = useState("100%");
+    const [isPanning, setIsPanning] = useState(false);
+    const viewProgress = useRef(scrollYProgress);
+
+    const lastScrollY = useRef(scrollYProgress.get());
+    const scrollPercent = useRef(null);
 
     // useEffect(() => {
     //     const updateScrollPercent = () => {
@@ -28,20 +32,24 @@ export default function Track() {
     //     };
 
     //     updateScrollPercent(); 
-        
+
     // }, []);
     const transformX = useTransform(scrollYProgress, [0, 1], [`21.3%`, `-67.1%`]);
     const transformImgX = useTransform(scrollYProgress, [0, 1], [`100%`, `0%`]);
-    
+
     const [height, setHeight] = useState('auto');
     const [opacity, setOpacity] = useState(0);
     useEffect(() => {
-        setTrackX(transformX);
-        setImageX(transformImgX);
-        console.log("changes occuring");
-        
-       
-    }, [transformX, transformImgX])
+        if (!isPanning) {
+            lastScrollY.current = scrollYProgress.get();
+            setTrackX(transformX);
+            setImageX(transformImgX);
+            console.log("changes occuring");
+        }
+
+
+
+    }, [scrollYProgress, isPanning])
 
     // useEffect(()=>{
     //     animate(".img-list", {x:`${transformX}`}, {duration:0.12, ease:'easeInOut'})
@@ -52,30 +60,69 @@ export default function Track() {
         const updateHeight = () => {
             if (divRef.current) {
                 const width = divRef.current.scrollWidth;
-                setHeight(`${width*5}px`)
+                setHeight(`${width * 5}px`)
                 setOpacity(1)
-            }   
+            }
         }
         updateHeight();
         window.addEventListener('resize', updateHeight)
-        
-        return () => window.removeEventListener('resize', updateHeight)
-    })
 
+        return () => window.removeEventListener('resize', updateHeight)
+    }, [])
+
+
+    function handlePanStart() {
+        setIsPanning(true)
+        console.log("panning start")
+    }
+    function handlePanEnd(event, panInfo) {
+        let panWidth = window.innerWidth / 2;
+        let offset = Math.max(Math.min(100, panInfo.offset.x / panWidth * 100), -100)
+        let scrollableHeight = divRef.current.scrollHeight - window.innerHeight;
+        let newProgress = Math.max(0, Math.min(1, (lastScrollY.current + offset / 100)));
+        window.scrollTo(0, newProgress*scrollableHeight);
+        console.log("panning end")
+        setIsPanning(false)
+    }
+    function handlePan(event, panInfo) {
+        if (!isPanning) return;
+        let panWidth = window.innerWidth / 2;
+        let offset = Math.max(Math.min(100, panInfo.offset.x / panWidth * 100), -100)
+        let baseTrackX = 21.3 + ((-88.4) * lastScrollY.current);
+        let baseImageX = 100 - (100 * lastScrollY.current);
+        let newTrackX = Math.max(-67.1, Math.min(21.3, baseTrackX + offset*0.882))
+        let newImageX = Math.max(0, Math.min(100, baseImageX + offset))
+        setTrackX(`${newTrackX}%`);
+        setImageX(`${newImageX}%`);
+
+        
+    }
 
     return (
-        <div className="img-track-div" ref={divRef} style={{ height, width: '100%',}}>
+        <motion.div className="img-track-div" ref={divRef} style={{ height, width: '100%', }} onPanStart={handlePanStart}
+            onPan={handlePan}
+            onPanEnd={handlePanEnd}>
             <div className="img-track" ref={scope}>
-                <motion.ul className="img-list" initial={{x:'21.3%'}} style={{x:trackX}}
+                <motion.ul className="img-list" initial={{ x: '21.3%' }} style={{ x: trackX }} 
+                    transition={{
+                        type: "tween",
+                        ease: "linear",
+                        duration: 0
+                    }}
                 >
                     {imgList.map((image, index) => (
-                        <li key={index} ><motion.img layout src={image} style={{objectPosition:imageX, opacity }}
+                        <li key={index} ><motion.img layout src={image} style={{ objectPosition: imageX, opacity }} 
+                        transition={{
+                            type: "tween",
+                            ease: "linear",
+                            duration: 0
+                        }}
                         /></li>
                     ))}
                 </motion.ul>
 
             </div>
-        </div>
+        </motion.div>
     )
 
 
