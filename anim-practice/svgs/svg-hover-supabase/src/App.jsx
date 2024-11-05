fe4932305b4b5389e3e482f33359795a40946a1e
William Jin <jinwilliam.jin@gmail.com>
2024-11-04 23:35:20 -0500

    new hover

---
+++ anim-practice/svgs/svg-hover-supabase/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import {CircleStackIcon} from '@heroicons/react/24/outline'
+import './App.css'
+
+function App() {
+
+  return(
+    <div className='main'>
+      <CircleStackIco className="icon" />
+    </div>
+  )
+}
+
+export default App
---
+++ anim-practice/svgs/svg-hover-supabase/src/components/svg-hover.jsx
@@ -0,0 +1,6 @@
+import { flameSvg } from "./svg";
+import "./svg-hover.css"
+import {motion} from 'framer-motion'
+export default function SvgHover() {
+
+}
